63911b936d2f24cab62c5a25b7c493845d4fe6e8
drmacdou <drmacdou@ualberta.ca>
2025-09-21 23:45:12 -0600

    check.sh

--- check.sh
+++ check.sh
@@ -2,5 +2,17 @@
 # Exit the script if any commands return with non-zero code
 set -e
 # DO NOT MODIFY ANY OF THE LINES ABOVE
+mkdir -p bin
 
-# TODO: Fill in this file with commands to compile, execute, and check output
+gcc -Wall -Werror -std=c99 -lm src/ex2q1.c -o bin/ex2q1
+
+for testcase in Testcases/2-*-input.txt; do
+
+    base=$(basename "$testcase" -input.txt)
+    
+
+    ./bin/ex2q1 < "$testcase" >| "Testcases/${base}-student.txt"
+    
+
+    diff -u "Testcases/${base}-output.txt" "Testcases/${base}-student.txt"
+done
